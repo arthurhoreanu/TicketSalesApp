1e03ca999e3f049982ddafa5a6472248324ff67e
scotiana2415 <scotiana2004@gmail.com>
2024-11-06 09:09:40 +0200

    proba commit

---
+++ src/model/Seat.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Seat {
+    private int seatID;
+    //private Section section;
+    private int rowNumber;
+    private int sitNumber;
+    private boolean reservedForEvent;
+}
