f220140b64820a3813aa021f9754bfef9fea5185
arthurhoreanu <arthur.horeanu@gmail.com>
2024-11-06 12:24:00 +0200

    Added LineUp connection classes

---
+++ src/model/ConcertLineUp.java
@@ -0,0 +1,27 @@
+package model;
+
+public class ConcertLineUp {
+    private int eventId;
+    private int artistId;
+
+    public ConcertLineUp(int eventId, int artistId) {
+        this.eventId = eventId;
+        this.artistId = artistId;
+    }
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
+    }
+
+    public int getArtistId() {
+        return artistId;
+    }
+
+    public void setArtistId(int artistId) {
+        this.artistId = artistId;
+    }
+}
---
+++ src/model/SportsEventLineUp.java
@@ -0,0 +1,27 @@
+package model;
+
+public class SportsEventLineUp {
+    private int eventId;
+    private int athleteID;
+
+    public SportsEventLineUp(int eventId, int artistId) {
+        this.eventId = eventId;
+        this.athleteID = artistId;
+    }
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
+    }
+
+    public int getArtistId() {
+        return athleteID;
+    }
+
+    public void setArtistId(int artistId) {
+        this.athleteID = artistId;
+    }
+}
