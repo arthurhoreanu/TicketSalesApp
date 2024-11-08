dd967bb6dfc982c3dd85f887e93f78297b0c0cc5
scotiana2415 <scotiana2004@gmail.com>
2024-11-08 19:33:48 +0200

    solved getSeat(care a fost inghitid de iad din clasa Section) problem :')

--- src/model/Section.java
+++ src/model/Section.java
@@ -1,21 +1,20 @@
 package model;
-
 import java.util.List;
+
 
 public class Section implements Identifiable {
     private int sectionID;
     private String sectionName;
     private int sectionCapacity;
     private Venue venue;
-    private List<Seat> seats;   //added for the return available seats
+    private List<Seat> seats;   //added for the return available seat
 
-    public Section(int sectionID, String sectionName, int sectionCapacity, Venue venue, List<Seat> seats) {
+    public Section(int sectionID, String sectionName, int sectionCapacity, Venue venue) {
         this.sectionID = sectionID;
         this.sectionName = sectionName;
         this.sectionCapacity = sectionCapacity;
         this.venue = venue;
-        this.seats = seats;
-
+        this.seats =  seats;
     }
 
     @Override
@@ -23,17 +22,16 @@ public class Section implements Identifiable {
         return this.sectionID;
     }
 
-    public List<Seat> getSeats(){
-        return seats;
-    }
-
-
     public Venue venue() {
         return venue;
     }
 
     public void setVenue(Venue venue) {
         this.venue = venue;
+    }
+
+    public List<Seat> getSeats(){
+        return seats;
     }
 
     public void setSectionID(int sectionID) {
