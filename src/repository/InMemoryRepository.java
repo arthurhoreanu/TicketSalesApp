bab0da8f6cadb28f51eaebd01f22263f38d2585a
arthurhoreanu <arthur.horeanu@gmail.com>
2024-11-12 21:17:59 +0200

    Wrote JavaDocs for the repository package

--- src/repository/InMemoryRepository.java
+++ src/repository/InMemoryRepository.java
@@ -9,28 +9,47 @@ import java.util.Map;
 public class InMemoryRepository<T extends Identifiable> implements IRepository<T> {
     private final Map<Integer, T> data = new HashMap<>();
 
+    /**
+     * Adds an object to the repository if it does not already exist.
+     * @param obj The object to be added to the repository.
+     */
     @Override
     public void create(T obj) {
         data.putIfAbsent(obj.getID(), obj);
     }
 
-    // Since read is void, we remove the return statement
+    /**
+     * Retrieves an object from the repository by its ID.
+     * @param id The ID of the object to be retrieved.
+     */
     @Override
     public void read(Integer id) {
-        // No return value - this method now effectively does nothing meaningful
-        data.get(id); // This line is redundant and doesn't have any effect
+        data.get(id);
     }
 
+    /**
+     * Updates an existing object in the repository.
+     * @param obj The object to be updated in the repository.
+     */
     @Override
     public void update(T obj) {
         data.replace(obj.getID(), obj);
     }
 
+    /**
+     * Removes an object from the repository by its ID.
+     * @param id The ID of the object to be removed.
+     */
     @Override
     public void delete(Integer id) {
         data.remove(id);
     }
 
+    /**
+     * Retrieves all objects from the repository.
+     * The list will contain all values from the repository, or an empty list if no objects are stored.
+     * @return A list containing all the objects currently stored in the repository.
+     */
     @Override
     public List<T> getAll() {
         return data.values().stream().toList();
